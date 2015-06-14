f2887cfc5f8f17d0a24d18645d981fab71c04198
Esteban Benavides <ebenavidescruz@gmail.com>
2015-06-13 22:14:54 -0600

    Pruebas con TwitterAPI
    Extracción de tweets

---
+++ TwitterAPI/MyAPI.py
@@ -0,0 +1,8 @@
+from TwitterAPI import TwitterAPI
+
+twitter_api = TwitterAPI('1uQQd0wTVH76kCPCqNVnm15Lk',
+                         'XNTLr9Y2S1ThRYlGQyKxoOkIVUuheAMrHFmpyFwIQZiyBKU8hT','898572810-btveERSAL1n5BflyJL54ZzylIKFeV4xA14vXPcaq','5bO2B8PsxiPiQeGaUL0ESGoZSCTMa4n2tvNkaZ39J1tYZ')
+
+_request = twitter_api.request('search/tweets', {'q':'PelonArmy'})
+for tweet in _request:
+        print(tweet)
