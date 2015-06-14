7d2ce77ae061e7d33208e0c4faf29fd5a477641c
Esteban Benavides <ebenavidescruz@gmail.com>
2015-06-13 23:09:22 -0600

    Comments

--- TwitterAPI/_TwitterSearch.py
+++ TwitterAPI/_TwitterSearch.py
@@ -1,12 +1,12 @@
 from TwitterSearch import *
 
 try:
-    tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['PelonArmy', 'LeagueOfLegends', 'Lindsan']) # let's define all words we would like to have a look for
-    tso.set_language('es') # we want to see German tweets only
-    tso.set_include_entities(False) # and don't give us all those entity information
+    tso = TwitterSearchOrder() 
+    tso.set_keywords(['PelonArmy', 'LeagueOfLegends', 'Lindsan']) 
+    tso.set_language('es') 
+    tso.set_include_entities(False) 
 
-    # it's about time to create a TwitterSearch object with our secret tokens
+
     ts = TwitterSearch(
         consumer_key = '1uQQd0wTVH76kCPCqNVnm15Lk',
         consumer_secret = 'XNTLr9Y2S1ThRYlGQyKxoOkIVUuheAMrHFmpyFwIQZiyBKU8hT',
@@ -14,9 +14,9 @@ try:
         access_token_secret = '5bO2B8PsxiPiQeGaUL0ESGoZSCTMa4n2tvNkaZ39J1tYZ'
      )
 
-     # this is where the fun actually starts :)
+
     for tweet in ts.search_tweets_iterable(tso):
         print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
 
-except TwitterSearchException as e: # take care of all those ugly errors if there are some
+except TwitterSearchException as e: 
     print(e)
